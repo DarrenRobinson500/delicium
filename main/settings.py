93d290468e129a9892b13ccf41818339b3bc6e96
Darren Robinson <darrenandamanda.robinson@gmail.com>
2024-01-05 14:40:29 +1100

    Attempting to fix ckeditor (attempt 2 - added ckeditor to STATICFILES_DIR)

--- main/settings.py
+++ main/settings.py
@@ -153,6 +153,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'ckeditor'),
+    os.path.join(BASE_DIR, 'ckeditor_uploader'),
+
     # BASE_DIR / 'static',
 ]
 
